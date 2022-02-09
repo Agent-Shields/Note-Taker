1e584bbe8b89187680843383a6c9b5711001ece1
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-02-08 19:01:11 -0600

    struggling with server

--- server.js
+++ server.js
@@ -2,11 +2,17 @@ const express = require('express');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
+const [{ db }] = require('./db/db.json')
 
 // express middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+
+// get request to db.json
+app.get('/api/db', (req,res) => {
+  res.json(db[0])
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
