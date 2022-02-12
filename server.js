b05d1fac7d93bcbdd46c3793271d77f72943fa13
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-02-12 11:42:32 -0600

    notes html now resolves properly

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require('express');
-
+const path = require('path');
 const { notes }  = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -14,9 +14,11 @@ app.get('/api/notes', (req,res) => {
   res.json(notes);
 })
 
+// get request send notes html
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
+
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
