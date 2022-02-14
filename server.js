46e64dfac0adf2b498488c0bf41f55ebc3a99854
Johnathan Shields <Johnathan.Shields@buhlergroup.com>
2022-02-13 21:36:34 -0600

    all requirements met, notes populate from left to right when clicked on left

--- server.js
+++ server.js
@@ -15,25 +15,31 @@ app.get('/api/notes', (req,res) => {
   res.json(notes);
 })
 
+// get request for a single note by ID
+app.get('/api/notes/:id', (req,res) => {
+  const result = findById(req.params.id, notes)
+  res.json(result);
+})
+
 // get request send notes html
 app.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, './public/notes.html'));
 });
 
-// get else requests send index.html
-app.get('*', (req,res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'))
-})
-
 // post request send notes
 app.post('/api/notes', (req,res) => {
   var newNote = req.body;
   var notes = JSON.parse(fs.readFileSync('./db/db.json'));
+  req.body.id = notes.length.toString();
   notes.push(newNote);
   fs.writeFileSync('./db/db.json', JSON.stringify(notes));
   res.json(notes);
 })
 
+// get else requests send index.html
+app.get('*', (req,res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'))
+})
 
 app.listen(PORT, () => {
     console.log(`API server now on port ${PORT}!`);
